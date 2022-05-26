a563f7b770ba041226a5a1a686c88bf0115b8df0
SantinoMajur <dem65109@gmail.com>
2022-05-26 17:42:06 +0300

    Added new content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     extend: {
       width: {
         '66':'46%'
+      },
+      maxWidth: {
+        '235': '235px',
+        '350': '350px',
+        '150': '150px'
+      },
+      minHeight: {
+        '300': '300px',
+        '270': '270px'
+      },
+      screens: {
+        'xs': '350px'
       }
     },
   },
